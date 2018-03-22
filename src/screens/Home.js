91bd5c2e621eab23cb0bd277c0cbd81a7a0e9b5c
Septian A Tama <sfirst.tama@gmail.com>
2018-03-23 00:18:36 +0700

    styling container and section title

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import { View, Text, FlatList, Image, Button, StyleSheet } from 'react-native';
 
 class Home extends Component {
+  static navigationOptions = {
+    headerTitle: 'TamvanShop',
+  }
+
   render() {
     return (
-      <View>
-        <Text>Product List</Text>
+      <View style={ styles.container }>
+        <Text style={ styles.sectionTitle }>Product List</Text>
         
       </View>   
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 16,
+  },
+  sectionTitle: {
+    fontSize: 24,
+  }
+})
 
 export default Home;
