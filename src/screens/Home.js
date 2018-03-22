2c1f50a55fe35cf7a165839bf08a885ec3f5a8f2
Septian A Tama <sfirst.tama@gmail.com>
2018-03-23 00:01:16 +0700

    initiating product list component

---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text, FlatList, Image, Button, StyleSheet } from 'react-native';
+
+class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Product List</Text>
+        
+      </View>   
+    );
+  }
+}
+
+export default Home;
