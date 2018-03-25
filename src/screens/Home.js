dba220b1c0b5fc62e3d64bd0623d58995552dcaa
Septian A Tama <sfirst.tama@gmail.com>
2018-03-25 23:12:36 +0700

    reduxify home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,34 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, Image, Button, TextInput, StyleSheet } from 'react-native';
+import { 
+  View,
+  Text,
+  FlatList,
+  Image,
+  Button,
+  TextInput,
+  StyleSheet 
+} from 'react-native';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { loadProducts } from '../store/products/products.actions';
 
 import ItemCard from '../components/ItemCard';
 
 class Home extends Component {
-  state = {
-    products: [
-      { id: 0, name: 'Elephone E-1 in Ear', price: 700, image: 'https://doki.co.id/image/cache/data/Handphone/Motorola/Moto%20z/Motorola-moto-z-black-500x500.jpg' },
-      { id: 0, name: 'JBL Wireless Earphone', price: 500, image: 'https://doki.co.id/image/cache/data/Handphone/xiaomi/Note%205A/Redmi-note-5a-Grey-500x500.jpg' },
-      { id: 0, name: 'Xiaomi Mi A1', price: 1000, image: 'https://doki.co.id/image/cache/data/Accessories/Tempered Glass/Tempered-Samsung-Galaxy-s8-s8+-500x500h.jpg' },
-      { id: 0, name: 'Samsung Galaxy S8', price: 7000, image: 'https://doki.co.id/image/cache/data/Handphone/Elephone/s1/Elephone-s1-black-doki-500x500.jpg' },
-    ]
-  }
-
   static navigationOptions = {
     headerTitle: 'TamvanShop',
   }
 
-  _keyExtractor = (item) => `product_${item.id}`
+  _keyExtractor = (item) => `product_${item.id}`;
 
   _renderItem = ({item}) => (
     <ItemCard item={ item } />
-  )
+  );
 
   render() {
     return (
       <View style={ styles.container }>
         <Text style={ styles.sectionTitle }>Product List</Text>
         <FlatList 
-          data={ this.state.products }
+          data={ this.props.products }
           renderItem={ this._renderItem }
           keyExtractor={ this._keyExtractor }
         />
@@ -47,4 +50,12 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Home;
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  loadProducts
+}, dispatch);
+
+const mapStateToProps = (state) => ({
+  products: state.products
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
