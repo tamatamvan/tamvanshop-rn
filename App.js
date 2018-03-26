ee7c47f79db1a89b0247b7a461e34b7c99f4bda4
Septian A Tama <sfirst.tama@gmail.com>
2018-03-26 16:35:19 +0700

    add shopping cart button

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
 import React from 'react';
+import {
+  TouchableHighlight,
+} from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 import store from './src/store';
 
+import { MaterialIcons } from '@expo/vector-icons';
+
 import Home from './src/screens/Home';
+import Cart from './src/screens/ShopingCart';
 
 const RootStack = StackNavigator({
   Home: {
     screen: Home,
   },
+  Cart: {
+    screen: Cart,
+  },
 }, {
   initialRouteName: 'Home',
-  navigationOptions: {
+  navigationOptions: ({ navigation }) => ({
     headerStyle: {
       backgroundColor: '#2962FF',
     },
     headerTintColor: '#fff',
-  },
+    headerRight: (
+      <TouchableHighlight onPress={ () => navigation.navigate('Cart') }>
+        <MaterialIcons name='shopping-cart' size={32} color='#fff'/>
+      </TouchableHighlight>
+    ),
+  }),
+  headerMode: 'screen',
 });
 
 export default class App extends React.Component {
---
+++ src/screens/ShopingCart.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { 
+  View,
+  Text,
+  FlatList,
+} from 'react-native';
+
+class ShopingCart extends Component {
+  render() {
+    return (
+      <View style={ styles.container }>
+        <Text style={ styles.sectionTitle }>Ini Shopping Cart</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 16,
+  },
+  sectionTitle: {
+    fontSize: 24,
+  },
+});
+
+export default ShopingCart;
