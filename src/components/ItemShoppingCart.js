75d734a166d74cd5e0a2199e4e97563c972fc97c
Septian A Tama <sfirst.tama@gmail.com>
2018-03-28 01:19:07 +0700

    moving stylesheet

--- src/components/ItemShoppingCart.js
+++ src/components/ItemShoppingCart.js
@@ -11,29 +11,12 @@ class ItemShoppingCart extends Component {
   render() {
     const { item } = this.props;
     return (
-      <View style={{
-        flex: 1,
-        paddingTop: 4,
-        paddingBottom: 4,
-        marginTop: 4,
-        marginBottom: 4,
-        flexDirection: 'row',
-        backgroundColor: '#fff'
-      }}>
+      <View style={ styles.itemCard}>
         <Image source={{ uri: item.image }} 
-         style={{ 
-          width: 75,
-          height: 75,
-         }}
+         style={ styles.imageSize }
         />
-        <View style={{
-          paddingLeft: 8,
-          paddingRight: 8,
-        }}>
-          <Text style={{ 
-            fontSize: 16,
-            fontWeight: 'bold'
-          }}>
+        <View style={ styles.itemInfo }>
+          <Text style={ styles.item }>
             { item.name } - US${ item.price } x { item.qty }
           </Text>
           <Text>Subtotal: US${ item.subtotal }</Text>
@@ -42,5 +25,29 @@ class ItemShoppingCart extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  itemCard: {
+    flex: 1,
+    paddingTop: 4,
+    paddingBottom: 4,
+    marginTop: 4,
+    marginBottom: 4,
+    flexDirection: 'row',
+    backgroundColor: '#fff'
+  },
+  imageSize: { 
+    width: 75,
+    height: 75,
+  },
+  itemInfo: {
+    paddingLeft: 8,
+    paddingRight: 8,
+  },
+  itemTitle: { 
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+})
 
 export default ItemShoppingCart;
