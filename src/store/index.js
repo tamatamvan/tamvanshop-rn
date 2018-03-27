43f59e9d80a5457f80b1593b818ba8575af81558
Septian A Tama <sfirst.tama@gmail.com>
2018-03-27 21:42:24 +0700

    inserting carts reducers to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import productsReducers from './products/products.reducers';
+import cartsReducers from './carts/carts.reducers';
 
 const reducers = combineReducers({
   products: productsReducers,
+  carts: cartsReducers,
 });
 
 const store = createStore(reducers);
