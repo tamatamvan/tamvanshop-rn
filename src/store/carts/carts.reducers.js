65f313ffb0118aca2714b96dbe4a8a5cccbb42d1
Septian A Tama <sfirst.tama@gmail.com>
2018-03-29 01:17:04 +0700

    when adding same item to cart, will increase the qty

--- src/store/carts/carts.reducers.js
+++ src/store/carts/carts.reducers.js
@@ -8,15 +8,35 @@ const initialState = {
   total: 0,
 }
 
+const idxInCart = (id, arr) => arr.findIndex(data => data.id === id)
+
 const cartsReducers = (state={ ...initialState }, action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      const item = {
-        ...action.payload,
-        subtotal: action.payload.price * action.payload.qty,
-      };
-      const updatedItems = [ ...state.items, item ];
-      const updatedTotal = state.total + item.subtotal;
+      const itemIdx = idxInCart(action.payload.id, state.items);
+      let item;
+      let updatedItems;
+      let updatedTotal;
+      if (itemIdx > -1) {
+        const selectedItem = state.items[itemIdx];
+        const additionalSubtotal = action.payload.price * action.payload.qty;
+        item = {
+          ...selectedItem,
+          qty: selectedItem.qty + action.payload.qty,
+          subtotal: selectedItem.subtotal + additionalSubtotal,
+        }
+        updatedItems =  [ ...state.items ];
+        updatedItems.splice(itemIdx, 1, item)
+        updatedTotal = state.total - state.items[itemIdx].subtotal + item.subtotal;
+      } else {
+        item = {
+          ...action.payload,
+          subtotal: action.payload.price * action.payload.qty,
+        };
+        updatedItems =  [ ...state.items, item ];
+        updatedTotal = state.total + item.subtotal;
+      }
+      
       return ({
         ...state,
         items: updatedItems,
